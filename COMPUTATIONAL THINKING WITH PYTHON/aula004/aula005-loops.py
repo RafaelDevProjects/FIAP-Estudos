9b9e24267cca6132711b939d11593b677e708e17
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-09-13 09:40:34 -0300

    aula005

--- COMPUTATIONAL THINKING WITH PYTHON/aula004/aula005-loops.py
+++ COMPUTATIONAL THINKING WITH PYTHON/aula004/aula005-loops.py
@@ -2,7 +2,7 @@
 
 
 # senha com while.
-senha = '1234'
+'''senha = '1234'
 senha_usuario = input('Diga sua senha: ')
 repetições = 0
 
@@ -14,4 +14,23 @@ if senha_usuario == senha:
     print('Acesso permitido')
 else:
     print('Acesso Bloquado')
+'''
+
+
+inicio = int(input('Digite um numero para começar: '))
+fim = int(input('Digite um numero para finalizar: '))
+
+if fim > inicio:
+    while inicio <= fim:
+        if inicio % 2 == 0:
+            print(f'O numero {inicio} é par')
+
+        else:
+            print(f'O numero {inicio} é impar')
+        inicio += 1
+else:
+    print('Tente novamente...')
+
+
+
 
