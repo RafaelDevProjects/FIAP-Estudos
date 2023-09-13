6e921d481ebb6045a4074d47d9f9a6979dffab8b
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-09-13 09:16:57 -0300

    Update aula005-loops.py

--- COMPUTATIONAL THINKING WITH PYTHON/aula004/aula005-loops.py
+++ COMPUTATIONAL THINKING WITH PYTHON/aula004/aula005-loops.py
@@ -4,13 +4,11 @@
 # senha com while.
 senha = '1234'
 senha_usuario = input('Diga sua senha: ')
-repetições = 1
+repetições = 0
 
 while senha != senha_usuario and repetições < 3:
-    print('Errou')
-    print(f'Tentativa {repetições}')
+    print(f'Tente novamente....\nTentativa {repetições + 1}')
     senha_usuario = input('Diga sua senha: ')
-
     repetições += 1
 if senha_usuario == senha:
     print('Acesso permitido')
