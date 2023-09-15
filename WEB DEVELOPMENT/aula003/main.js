803c5053fad385a31d3d25a56d178624c3eaa644
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-09-15 09:45:29 -0300

    aula003

---
+++ WEB DEVELOPMENT/aula003/main.js
@@ -0,0 +1,16 @@
+alert('Ola mundo')
+/* Comentario */
+// comentario
+
+
+document.write('Olaaaaaa');
+
+console.log('Usando o console');   
+
+
+// Fazer a variavel aparecer no console
+let nome = 'Rafael';
+let sobrenome ="Almeida";
+
+console.log(nome)
+console.log(sobrenome)
