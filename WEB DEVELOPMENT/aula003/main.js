9a36ac6039f4d01f0d07087cc0d0d73c58c65253
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-09-15 10:13:05 -0300

    Update main.js

--- WEB DEVELOPMENT/aula003/main.js
+++ WEB DEVELOPMENT/aula003/main.js
@@ -10,7 +10,7 @@ console.log('Usando o console');
 
 // Fazer a variavel aparecer no console
 let nome = 'Rafael';
-let sobrenome ="Almeida";
+let sobrenome = "Almeida";
 
 console.log(nome)
 console.log(sobrenome)
