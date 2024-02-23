95491d40ad590704f53fb1f4e2925028da5a5afa
Rafael Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-02-23 11:52:50 -0300

    aula002

--- WEB_DEVELOPMENT/semestre_2/aula002/script.js
+++ WEB_DEVELOPMENT/semestre_2/aula002/script.js
@@ -1,6 +1,7 @@
 const $divListaFilmes = document.querySelector('#listaFilmes');
 const btn1 = document.querySelector('button')
 const $filmeInput = document.querySelector('#filmeInput')
+const titulo = document.querySelector('h1')
 
 const $itemLista = document.createElement('li');
 btn1.addEventListener('click', () =>{
@@ -8,4 +9,6 @@ btn1.addEventListener('click', () =>{
     $divListaFilmes.append($itemLista);
     //adiciona texto dinamico ao elemento
     $itemLista.innerHTML = $filmeInput.value
+    titulo.innerHTML($itemLista)
+
 })
