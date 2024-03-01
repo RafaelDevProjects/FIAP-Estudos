e0cc9f4e21fdd4b3a7567a7ca5142f8f86af8f71
Rafael Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-03-01 09:41:39 -0300

    aula de arrays

--- WEB_DEVELOPMENT/semestre_2/aula002/script.js
+++ WEB_DEVELOPMENT/semestre_2/aula002/script.js
@@ -1,14 +1,29 @@
-const $divListaFilmes = document.querySelector('#listaFilmes');
-const btn1 = document.querySelector('button')
-const $filmeInput = document.querySelector('#filmeInput')
-const titulo = document.querySelector('h1')
+// const $divListaFilmes = document.querySelector('#listaFilmes');
+// const btn1 = document.querySelector('button')
+// const $filmeInput = document.querySelector('#filmeInput')
+// const titulo = document.querySelector('h1')
 
-const $itemLista = document.createElement('li');
-btn1.addEventListener('click', () =>{
-    //adiciona a lista
-    $divListaFilmes.append($itemLista);
-    //adiciona texto dinamico ao elemento
-    $itemLista.innerHTML = $filmeInput.value
-    titulo.innerHTML($itemLista)
+// const $itemLista = document.createElement('li');
+// btn1.addEventListener('click', () =>{
+//     //adiciona a lista
+//     $divListaFilmes.append($itemLista);
+//     //adiciona texto dinamico ao elemento
+//     $itemLista.innerHTML = $filmeInput.value
+//     titulo.innerHTML($itemLista)
 
-})
+// })
+
+const listaFilmes = document.querySelector("#listaFilmes")
+// // com strings
+ let filmes = ['mario', 'maria', 'joana', 'harry', 'potter']
+ filmes.push('Senhor')
+ filmes.pop()
+ filmes.push('Senhora')
+
+window.onload = () => {
+    for(let i = 0; i < filmes.length; i++){
+        let listaItem = document.createElement('li')
+        listaFilmes.append(listaItem)
+        listaItem.innerHTML = filmes[i]
+    }
+}
