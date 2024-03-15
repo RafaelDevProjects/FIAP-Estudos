372ed72f4acfdf91e1935466144010f64d1689c3
Rafael Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2024-03-15 09:48:37 -0300

    aula de react usando vite

---
+++ WEB_DEVELOPMENT/semestre_2/aula004/vite-project/components/banner/banner.jsx
@@ -0,0 +1,10 @@
+function Banner(){
+    return(
+        <header>
+            <h1>Componente Banner</h1>
+            <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+        </header>
+    )
+}
+
+export default Banner
