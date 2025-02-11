02107f130e6e93790d2238ddde296a1c990fd591
Rafael Sigoli <124459052+RafaelDevProjects@users.noreply.github.com>
2025-02-11 19:44:39 -0300

    criandoBranch

--- WEB_DEVELOPMENT/semestre_1/aula005/ternario.js
+++
@@ -1,6 +0,0 @@
-let numero = 7;
-
-// ternario faz uma condição, se for verdadeiro ele dara para,se for Falso ele da impar
-let resultado = (numero % 2 == 0) ? 'par':'impar';
-
-console.log(resultado)
--- WEB_DEVELOPMENT/semestre_1/aula006/exercicio-casa.js
+++
@@ -1,8 +0,0 @@
-const cities = ["são paulo", "rio de janeiro", "minas gerais"];
-let upperCaseCities = [];
-
-for (city of cities){
-    upperCaseCities.push(city.toUpperCase())
-}
-console.log(cities)
-console.log(upperCaseCities);
